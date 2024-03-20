f91facb62d9d0d46ba32f9392cd17c2dc5bf9689
Simon VDB <simonvdbroeck@gmail.com>
2024-03-21 00:30:40 +0100

    add flat routes pkg

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
-import { vercelPreset } from '@vercel/remix/vite';
+import { vercelPreset } from "@vercel/remix/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { flatRoutes } from "remix-flat-routes";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+      ignoredRouteFiles: ["**/*"],
+      routes(defineRoutes) {
+        return flatRoutes("routes", defineRoutes, {
+          ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}", "**/__*.*"],
+        });
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
